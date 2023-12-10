e50d510e612017654462f626eb548cfa6b96fe1a
deepak <deepamdeebu@gmail.com>
2023-12-10 13:38:10 +0530

    UseEffect : Another hook that listens and gets called on re-render . We can have it scoped on specific variable change .

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v1 as uuidv1 } from 'uuid';
 import NewSongForm from './NewSongForm';
 
@@ -8,19 +8,27 @@ const SongList = () => {
         { title: 'Song 2', id: 2 },
         { title: 'song 3', id: 3 }
     ]);
+    const [age, setAge] = useState(20);
     const addSong = (title) => {
         setSongs([...songs, { title: title, id: uuidv1() }]);
     };
+    useEffect(() => {
+        console.log('useEffect callback', songs);
+      }, [songs]);
+    useEffect(() => {
+        console.log('useEffect callback', age);
+      }, [age]);
     return (
-    <div className="song-list">
-        <ul>
-            {songs.map(song => {
-                return ( <li key={song.id}>{song.title}</li> );
-            })}
-        </ul>
-        <NewSongForm addSong={addSong} />
-    </div>
-  );
+        <div className="song-list">
+            <ul>
+                {songs.map(song => {
+                    return ( <li key={song.id}>{song.title}</li> );
+                })}
+            </ul>
+            <NewSongForm addSong={addSong} />
+            <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
+        </div>
+    );
 }
 
 export default SongList;
